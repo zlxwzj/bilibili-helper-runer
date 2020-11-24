1750267681aaa493e8d0785421f4a09e48d0c893
Kurenai <kurenai233@yahoo.com>
2020-11-24 19:34:29 +0800

    chore: 删除冗余方法

--- download.sh
+++ download.sh
@@ -1,18 +1,10 @@
 #!/bin/bash
 version=$(wget -qO- -t1 -T2 "https://api.github.com/repos/JunzhouLiu/BILIBILI-HELPER/releases/latest" | grep "tag_name" | head -n 1 | awk -F ":" '{print $2}' | sed 's/\"//g;s/,//g;s/ //g;s/V//g')
 
-function installUnzip(){
-  command -v apt >/dev/null 2>&1 && (apt-get update; apt-get install unzip -y; return;)
-  command -v yum >/dev/null 2>&1 && (yum install unzip -y; return;)
-}
+wget -O "BILIBILI-HELPER.zip" "https://glare.now.sh/JunzhouLiu/BILIBILI-HELPER/BILIBILI-HELPER-v${version}.zip"
+command -v unzip >/dev/null 2>&1 || installUnzip
+unzip -o "BILIBILI-HELPER.zip" -d "."
+rm "BILIBILI-HELPER.zip"
+mv "BILIBILI-HELPER-v${version}.jar" "BILIBILI-HELPER.jar" -f
 
-function download(){
-  wget -O "BILIBILI-HELPER.zip" "https://glare.now.sh/JunzhouLiu/BILIBILI-HELPER/BILIBILI-HELPER-v${1}.zip"
-  command -v unzip >/dev/null 2>&1 || installUnzip
-  unzip -o "BILIBILI-HELPER.zip" -d "."
-  rm "BILIBILI-HELPER.zip"
-  mv "BILIBILI-HELPER-v${1}.jar" "BILIBILI-HELPER.jar" -f
-}
-
-download "$version"
 echo "下载完成"

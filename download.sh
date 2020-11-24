dbe16167ce8760c4eef5a086ed5093fceeeaff21
Kurenai <kurenai233@yahoo.com>
2020-11-24 20:30:25 +0800

    fix: config.json doesn't work

--- download.sh
+++ download.sh
@@ -2,9 +2,11 @@
 version=$(wget -qO- -t1 -T2 "https://api.github.com/repos/JunzhouLiu/BILIBILI-HELPER/releases/latest" | grep "tag_name" | head -n 1 | awk -F ":" '{print $2}' | sed 's/\"//g;s/,//g;s/ //g;s/V//g')
 
 wget -O "BILIBILI-HELPER.zip" "https://glare.now.sh/JunzhouLiu/BILIBILI-HELPER/BILIBILI-HELPER-v${version}.zip"
-command -v unzip >/dev/null 2>&1 || installUnzip
-unzip -o "BILIBILI-HELPER.zip" -d "."
+mkdir "tmp"
+unzip -o "BILIBILI-HELPER.zip" -d "./tmp/"
+mv "./tmp/BILIBILI-HELPER-v${version}.jar" "BILIBILI-HELPER.jar" -f
 rm "BILIBILI-HELPER.zip"
-mv "BILIBILI-HELPER-v${version}.jar" "BILIBILI-HELPER.jar" -f
+rm -rf "./tmp/"
+rmdir "tmp"
 
 echo "下载完成"

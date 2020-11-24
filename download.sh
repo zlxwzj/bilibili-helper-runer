fa8da410bf794cabd1bd12a24a8c00ba442890d1
Kurenai <kurenai233@yahoo.com>
2020-11-24 20:34:50 +0800

    feat: workflow add cache step

--- download.sh
+++ download.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 version=$(wget -qO- -t1 -T2 "https://api.github.com/repos/JunzhouLiu/BILIBILI-HELPER/releases/latest" | grep "tag_name" | head -n 1 | awk -F ":" '{print $2}' | sed 's/\"//g;s/,//g;s/ //g;s/V//g')
 
-wget -O "BILIBILI-HELPER.zip" "https://glare.now.sh/JunzhouLiu/BILIBILI-HELPER/BILIBILI-HELPER-v${version}.zip"
-mkdir "tmp"
-unzip -o "BILIBILI-HELPER.zip" -d "./tmp/"
-mv "./tmp/BILIBILI-HELPER-v${version}.jar" "BILIBILI-HELPER.jar" -f
+wget -O "BILIBILI-HELPER.zip" "https://github.com/JunzhouLiu/BILIBILI-HELPER/releases/download/V${version}/BILIBILI-HELPER-v${version}.zip"
+mkdir "jar"
+unzip -o "BILIBILI-HELPER.zip" -d "./jar/"
+mv "./jar/BILIBILI-HELPER-v${version}.jar" "./jar/BILIBILI-HELPER.jar" -f
+mv "./config.json" "./jar/config.json" -f
 rm "BILIBILI-HELPER.zip"
-rm -rf "./tmp/"
-rmdir "tmp"
 
 echo "下载完成"
